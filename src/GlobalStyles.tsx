b571e820416360bcaf162a32b42ec5b801fa3afe
mertyldrr <mertyildirir14@gmail.com>
2022-08-27 02:50:48 +0300

    GlobalStyles added

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,13 @@
+import { css } from "@emotion/react";
+
+const GlobalStyles = css`
+  * {
+    font-family: Montserrat, sans-serif;
+  }
+  p {
+    margin: 0px;
+    padding: 0px;
+  }
+`;
+
+export default GlobalStyles;
