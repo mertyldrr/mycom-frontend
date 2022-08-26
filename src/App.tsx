a8e47762744a7d317ed903d46b922fa7e5257e5d
mertyldrr <mertyildirir14@gmail.com>
2022-08-27 02:53:13 +0300

    Homepage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,18 @@
-import React from "react";
-import { Link } from "@mui/material";
-import { ReactComponent as TUM } from "./assets/TUM.svg";
+import Homepage from "./components/Homepage";
+import { Global } from "@emotion/react";
+import GlobalStyles from "./GlobalStyles";
 import "./App.css";
 
 function App() {
   return (
     <div className="app-container">
-      <div
-        className="homepage-container"
-        style={{
-          display: "flex",
-          flex: 1,
-          flexDirection: "column",
-          alignItems: "flex-start",
-          justifyContent: "center",
-          paddingLeft: 100,
-        }}
-      >
-        <p
-          style={{
-            fontSize: "7em",
-            fontWeight: 900,
-            color: "white",
-            margin: 0,
-            padding: 0,
-          }}
-        >
-          hello!
-        </p>
-        <p
-          style={{
-            fontSize: "3.5em",
-            fontWeight: 500,
-            color: "#F8F8FF",
-            width: "65%",
-          }}
-        >
-          I'm Mert, an Informatics Master's student{" "}
-          <span style={{ color: "#3070b3" }}>@</span>
-          <Link href="https://www.tum.de/en/" target={"_blank"}>
-            <TUM style={{ marginRight: 5 }} width={80} height={40} />
-          </Link>{" "}
-          and part-time frontend developer{" "}
-          <Link
-            href="https://smokeless.world/"
-            underline="none"
-            color="#0052b5"
-            target={"_blank"}
-          >
-            @Smokeless
-          </Link>
-        </p>
-        <p style={{ fontSize: "2.5em", fontWeight: 700, color: "#100C07", marginTop: 0 }}>
-          Currently working on React-Native, ReactJS, GraphQL, Redux
-        </p>
-        <p style={{ fontSize: "2em", fontWeight: 700, color: "#100C07", margin: 0 }}>
-          Have experience on Node.js, Python
-        </p>
+      <Global styles={GlobalStyles} />
+      <div className="navbar-container">
+        <p className="navbar-text">Home</p>
+        <p className="navbar-text">About</p>
+        <p className="navbar-text">Contact</p>
       </div>
+      <Homepage />
     </div>
   );
 }
---
+++ src/components/Homepage.tsx
@@ -0,0 +1,65 @@
+import styled from "@emotion/styled";
+import { Link } from "@mui/material";
+import { ReactComponent as TUM } from "../assets/TUM.svg";
+
+const Homepage = () => {
+  return (
+    <Container>
+      <Hello>hello!</Hello>
+      <AboutMe>
+        I'm Mert, an Informatics Master's student
+        <span style={{ color: "#3070b3" }}>@</span>
+        <Link href="https://www.tum.de/en/" target={"_blank"}>
+          <TUM style={{ marginRight: 5 }} width={80} height={40} />
+        </Link>
+        and part-time frontend developer
+        <Link
+          href="https://smokeless.world/"
+          underline="none"
+          color="#0052b5"
+          target={"_blank"}
+        >
+          @Smokeless
+        </Link>
+      </AboutMe>
+      <Technologies>
+        Currently working on React-Native, ReactJS, GraphQL, Redux
+      </Technologies>
+      <Technologies>Also, have experience on Node.js, Python</Technologies>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: #c0c8d5;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding-left: 100px;
+`;
+
+const Hello = styled.p`
+  font-size: 8em;
+  font-weight: 900;
+  color: white;
+`;
+
+const AboutMe = styled.p`
+  font-size: 3.5em;
+  font-weight: 700;
+  color: #f8f8ff;
+  width: 80%;
+  padding-top: 30px;
+  padding-bottom: 20px;
+`;
+
+const Technologies = styled.p`
+  font-size: 3em;
+  font-weight: 700;
+  color: #100c07;
+  padding-top: 20px;
+`;
+
+export default Homepage;
