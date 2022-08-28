4f9925820cc93aae5ae230bcebfac789569a1222
mertyldrr <mertyildirir14@gmail.com>
2022-08-28 03:02:19 +0300

    BrowserRouter and Routes (react-router-v6) added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 import { Global } from "@emotion/react";
 import GlobalStyles from "./GlobalStyles";
 import "./App.css";
@@ -9,7 +10,9 @@ function App() {
     <div className="app-container">
       <Global styles={GlobalStyles} />
       <Navbar />
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+      </Routes>
     </div>
   );
 }
