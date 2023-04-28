ff8c963cf98d90600ed68c306b8422158d4ae6ed
mertyldrr <mertyildirir14@gmail.com>
2023-04-28 16:52:59 +0200

    remove globalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import Homepage from "./components/Homepage";
 import { Routes, Route } from "react-router-dom";
-import { Global } from "@emotion/react";
-import GlobalStyles from "./GlobalStyles";
-import "./App.css";
-import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Box>
-      <Global styles={GlobalStyles} />
+    <div className="flex">
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         {/* <Route path="/photos" element={<PhotoGallery />}></Route> */}
       </Routes>
-    </Box>
+    </div>
   );
 }
 
