7251e66d8a21ed60858ae178453e3ce711d7647e
mertyldrr <mertyildirir14@gmail.com>
2023-04-28 16:52:50 +0200

    use env variables

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -1,24 +1,23 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import Gallery from "react-grid-gallery";
 
 const PhotoGallery = () => {
   const [photos, setPhotos] = useState<{ url: string }[]>();
   const fetchData = async () => {
-    const res = await axios.get(
-      `http://ec2-18-196-8-56.eu-central-1.compute.amazonaws.com/photos`
-    );
+    const res = await axios.get(`${process.env.REACT_APP_BASE_URL}\\photos`);
     setPhotos(res.data);
     console.log(res.data);
   };
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(photos && photos[0]);
   return (
     <div>
       {/* <Gallery images={IMAGES} backdropClosesModal={true} /> */}
-      {photos && photos.map((photo) => <img alt={""} src={photo.url}></img>)}
+      {photos &&
+        photos.map((photo, idx) => (
+          <img alt={""} key={idx} src={photo.url}></img>
+        ))}
     </div>
   );
 };
