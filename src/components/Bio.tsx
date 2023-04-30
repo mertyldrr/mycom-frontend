6ddf654cbd371f6efe9141bfa5c5e737b945e90c
mertyldrr <mertyildirir14@gmail.com>
2023-04-30 19:36:50 +0200

    add download cv

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -6,14 +6,35 @@ export const Bio = () => {
   const [icons, setIcons] = useState<S3Icons[]>();
   const [profilePicture, setProfilePictrue] = useState<S3Icons>();
   const fetchPlLogos = async () => {
-    const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/plangicons`);
-
+    const res = await axios.get(
+      `${process.env.REACT_APP_BACKEND_BASE_URL}/plangicons`
+    );
     setIcons(res.data);
   };
 
   const fetchProfilePicture = async () => {
-    const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/photos`);
+    const res = await axios.get(
+      `${process.env.REACT_APP_BACKEND_BASE_URL}/photos`
+    );
     setProfilePictrue(res.data[0]);
+  };
+
+  const downloadPdf = () => {
+    const pdfUrl = `${process.env.REACT_APP_SUBDOMAIN}/media/cv/mert_yildirir_cv.pdf`;
+    console.log(pdfUrl);
+    axios({
+      url: pdfUrl,
+      method: "GET",
+      responseType: "blob",
+    }).then((response) => {
+      console.log(response);
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "mert_yildirir_cv.pdf");
+      document.body.appendChild(link);
+      link.click();
+    });
   };
 
   useEffect(() => {
@@ -40,6 +61,19 @@ export const Bio = () => {
             I'm currently pursuing a Master's degree in Informatics at Technical
             University of Munich.
           </p>
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+            onClick={() => downloadPdf()}
+          >
+            <svg
+              className="fill-current w-4 h-4 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+            </svg>
+            <span className="font-mono">Download CV</span>
+          </button>
         </div>
       </div>
       <div className="w-full lg:w-4/5 flex flex-row justify-between items-center pt-12">
