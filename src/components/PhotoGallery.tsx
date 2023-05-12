2696b404480ad380aa95c1cb5025faae1c489374
mertyldrr <mertyildirir14@gmail.com>
2023-05-12 14:14:58 +0200

    comment out photo gallery[wip]

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -1,26 +1,27 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { S3Icons } from "../types";
+export {};
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { S3Media } from "../types";
 
-const PhotoGallery = () => {
-  const [photos, setPhotos] = useState<S3Icons[]>();
-  const fetchData = async () => {
-    const res = await axios.get(
-      `${process.env.REACT_APP_BACKEND_BASE_URL}\\photos`
-    );
-    setPhotos(res.data);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-  return (
-    <div>
-      {/* <Gallery images={IMAGES} backdropClosesModal={true} /> */}
-      {/* {photos &&
-        photos.map((photo, idx) => (
-          <img alt={""} key={idx} src={photo.url}></img>
-        ))} */}
-    </div>
-  );
-};
-export default PhotoGallery;
+// const PhotoGallery = () => {
+//   const [photos, setPhotos] = useState<S3Media[]>();
+//   const fetchData = async () => {
+//     const res = await axios.get(
+//       `${process.env.REACT_APP_BACKEND_BASE_URL}\\photos`
+//     );
+//     setPhotos(res.data);
+//   };
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+//   return (
+//     <div>
+//       {/* <Gallery images={IMAGES} backdropClosesModal={true} /> */}
+//       {/* {photos &&
+//         photos.map((photo, idx) => (
+//           <img alt={""} key={idx} src={photo.url}></img>
+//         ))} */}
+//     </div>
+//   );
+// };
+// export default PhotoGallery;
